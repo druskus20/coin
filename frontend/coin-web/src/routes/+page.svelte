f695090c28aba8d9c65ea5b5fbe940497c216250
druskus20 <druskus20@gmail.com>
2024-04-11 16:22:12 +0200

    feat: Leptos in CSR

--- frontend/coin-web/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script type="module">
-  import { init_and_run } from '$lib';
-  init_and_run();
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
