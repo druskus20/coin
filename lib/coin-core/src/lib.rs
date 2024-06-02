5ccb637d6ceeb8d7f8a8a027ee64f60402b502ed
drusk <dev@druskus.com>
2024-06-02 03:48:37 +0200

    skeleton

--- lib/coin-core/src/lib.rs
+++ lib/coin-core/src/lib.rs
@@ -1,25 +1,2 @@
-mod db;
 pub mod errors;
-
-use errors::Result;
-
-pub struct Coin {
-    db: db::Db,
-}
-
-impl Coin {
-    pub async fn try_new() -> Result<Coin> {
-        let db = db::Db::try_init().await?;
-        Ok(Self { db })
-    }
-
-    pub async fn add_expense(&self, amount: u32) -> Result<()> {
-        self.db.add_expense(amount).await?;
-        Ok(())
-    }
-
-    pub async fn get_expenses(&self) -> Result<Vec<db::Expense>> {
-        let expenses = self.db.get_expenses().await?;
-        Ok(expenses)
-    }
-}
+mod frankfurter;
