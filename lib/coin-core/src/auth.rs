ca8dfece49e2b297e0909389a50c5e4e4514223f
drusk <pedro@elk.audio>
2024-06-22 05:33:22 +0200

    feat: github authentication and credential management

--- lib/coin-core/src/auth.rs
+++ lib/coin-core/src/auth.rs
@@ -1,37 +1,89 @@
-use github_device_flow::{Credential, DeviceFlowError};
+use github_device_oauth::{Credentials, DeviceFlow};
 
-use crate::error::{self, GithubAuthError};
+use crate::error::CoinError;
+use keyring::Entry;
 
-pub(crate) struct GithubAuth {
-    client_id: String,
-    host: Option<String>,
+const HOST: &str = "github.com";
+const SCOPES: &str = "read:user";
+const APP_NAME: &str = "coin";
+
+async fn refresh_or_authorize_github(
     refresh_token: Option<String>,
+) -> Result<Credentials, CoinError> {
+    #[cfg(debug_assertions)]
+    match &refresh_token {
+        Some(refresh_token) => println!(
+            "Refreshing access token with refresh token {}",
+            refresh_token
+        ),
+        None => println!("Authorizing access token"),
+    }
+    let client_id = std::env::var("COIN_GITHUB_CLIENT_ID")?;
+    Ok(
+        DeviceFlow::new(client_id, HOST.to_string(), SCOPES.to_string())
+            .refresh_or_authorize(refresh_token)
+            .await?,
+    )
 }
 
-impl GithubAuth {
-    pub fn new(client_id: &str, host: Option<&str>, refresh_token: Option<&str>) -> Self {
-        Self {
-            client_id: client_id.to_string(),
-            host: host.map(|s| s.to_string()),
-            refresh_token: refresh_token.map(|s| s.to_string()),
+async fn store_credentials(credentials: Credentials) -> Result<(), CoinError> {
+    #[cfg(debug_assertions)]
+    println!("Storing credentials");
+    let username = whoami::username();
+    let entry = Entry::new(APP_NAME, &username)?;
+    tokio::task::spawn_blocking(move || -> Result<(), CoinError> {
+        entry.set_password(&credentials.try_to_string()?)?;
+        Ok(())
+    })
+    .await??;
+
+    Ok(())
+}
+
+async fn read_credentials() -> Result<Credentials, CoinError> {
+    let username = whoami::username();
+    let entry = Entry::new(APP_NAME, &username)?;
+
+    let password = tokio::task::spawn_blocking(move || -> Result<String, CoinError> {
+        match entry.get_password() {
+            Ok(password) => Ok(password),
+            Err(keyring::Error::NoEntry) => Err(CoinError::KeyringNoEntry),
+            Err(err) => Err(err.into()),
         }
-    }
-    pub fn authorize(&self) -> Result<Credential, GithubAuthError> {
-        Ok(github_device_flow::authorize(
-            self.client_id.clone(),
-            self.host.clone(),
-        )?)
+    })
+    .await??;
+
+    Ok(Credentials::try_from_string(&password)?)
+}
+
+pub struct CredentialsManager {
+    credentials: Credentials,
+}
+
+impl CredentialsManager {
+    pub async fn try_init_with_github() -> Result<Self, CoinError> {
+        let credentials = match read_credentials().await {
+            Ok(credentials) => refresh_or_authorize_github(Some(credentials.refresh_token)).await?,
+            Err(CoinError::KeyringNoEntry) => refresh_or_authorize_github(None).await?,
+            Err(err) => return Err(err),
+        };
+        store_credentials(credentials.clone()).await?;
+        Ok(CredentialsManager { credentials })
     }
 
-    pub fn refresh(&self) -> Result<Credential, GithubAuthError> {
-        if let Some(rt) = &self.refresh_token {
-            Ok(github_device_flow::refresh(
-                self.client_id.as_str(),
-                rt.as_str(),
-                self.host.clone(),
-            )?)
-        } else {
-            Err(DeviceFlowError::GitHubError("No refresh token found".to_string()).into())
+    pub async fn access_token_or_refresh(&mut self) -> Result<&str, CoinError> {
+        if is_expired(&self.credentials) {
+            self.credentials = refresh_or_authorize_github(None).await?;
+            store_credentials(self.credentials.clone()).await?;
         }
+        Ok(&self.credentials.access_token)
     }
+}
+
+const EXPIRATION_MARGIN: i64 = 120;
+fn is_expired(credentials: &Credentials) -> bool {
+    let ts = credentials.timestamp as i64;
+    let expires_in = credentials.expires_in as i64;
+    let now = chrono::Utc::now().timestamp();
+    ts + expires_in < now + EXPIRATION_MARGIN
 }
--- lib/coin-core/src/db.rs
+++ lib/coin-core/src/db.rs
@@ -1,7 +1,7 @@
-use crate::{auth::GithubAuth, error::DatabaseError};
+use crate::error::DatabaseError;
 use firebase_rs::Firebase;
 
-fn try_connect(auth_key: &str) -> Result<Firebase, DatabaseError> {
+pub async fn try_connect(auth_key: &str) -> Result<Firebase, DatabaseError> {
     Ok(Firebase::auth(
         "https://myfirebase.firebaseio.com",
         auth_key,
